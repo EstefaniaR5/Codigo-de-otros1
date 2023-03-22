928fa8515e96fb8b63ff6ed9fc1a41b1317cea4e
EstefaniaR5 <estefania.rams29@gmail.com>
2023-03-22 11:57:22 -0600

    correcion de codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,21 @@
+//Este programa esta utilizando un API de GitHub para obtener y mostraar los siguiente datos del usuario: (nombre del usuario, blog y localizacion)
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
+//faltaba un . a variables de querySelector
 
-function displayUser(username) {
+//se agrega async 
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.json();
+  //7manda error, se cambian comillas simples a ``
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
---
+++ readme.md
@@ -0,0 +1,13 @@
+Da instrucciones:
+20 min - Revisión de código de otros.
+Clona el proyecto de Github a tu computadora.
+Analiza el proyecto, para entender su funcionamiento. Pon mucha atención, el código contiene algunos errores.
+Aplica las buenas prácticas para refactoriza el código:
+Haz el código funcionar
+Mejora su legibilidad.
+Optimiza el código.
+Al final de la actividad:
+En la parte superior debes escribir en un comentario una breve descripción acerca del funcionamiento del código.
+El programa debe de funcionar.
+Debes documentar por medio de comentarios en el código los cambios que hiciste.
+Al terminar la sesión debes subir la prueba técnica a un repositorio de GitHub y compartirlo con el equipo de instrucción.
